d4b7460bf0c6bc47a76f9e0bfd0fae41e605e164
Andrea Pasqualini <andpsq@gmail.com>
2021-03-21 23:05:45 +0100

    Update to reflect change in CLI API

--- main.ps1
+++ main.ps1
@@ -1,2 +1,2 @@
-python ./download_sdi_data.py 1992 2019 ./data/
+python ./download_sdi_data.py ./data/ 1992 2019 6
 python ./sdi_dataset_assembler.py ./data/ ./pull_variables.json ./data/sdi.csv.xz
--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 ./download_sdi_data.py 1992 2019 ./data/
+python3 ./download_sdi_data.py ./data/ 1992 2019 6
 python3 ./sdi_dataset_assembler.py ./data/ ./pull_variables.json ./data/sdi.csv.xz
